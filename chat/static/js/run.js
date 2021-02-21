c673995fc0462da4664dba658f3de52af0440c86
ProfJAT <46096425+ProfJAT@users.noreply.github.com>
2021-02-20 16:14:14 -0800

    Chat templates

---
+++ chat/static/js/run.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    document.getElementById("board-bar").onclick = function() {
+        // Change arrow
+        var arrow = document.getElementById("arrow");
+        if (arrow.getAttribute("src") === "../images/up.png") {
+            arrow.src = "../images/down.png";
+        }
+        else {
+            arrow.src = "../images/up.png";
+        }
+
+        // Change whiteboard
+        var whiteboard = document.getElementById("whiteboard");
+        whiteboard.classList.toggle("invisible");
+        whiteboard.src = "https://awwapp.com/";
+    }
+}
