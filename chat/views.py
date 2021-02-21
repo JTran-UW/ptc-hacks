c86fa9cc4619fda9be1673463f0ed17ad15fe6fe
yut-code <78696249+yut-code@users.noreply.github.com>
2021-02-20 19:15:49 -0500

    p3

---
+++ chat/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+#def chat_view(request, *args, **kwargs):
+	#return render(request, "math.html", {})
