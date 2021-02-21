999f95a9669312a9fed186d87a3f0e15d2987c16
ProfJAT <46096425+ProfJAT@users.noreply.github.com>
2021-02-20 17:12:47 -0800

    Resolved static issues in chat

--- chat/static/js/run.js
+++ chat/static/js/run.js
@@ -2,11 +2,11 @@ window.onload = function() {
     document.getElementById("board-bar").onclick = function() {
         // Change arrow
         var arrow = document.getElementById("arrow");
-        if (arrow.getAttribute("src") === "../images/up.png") {
-            arrow.src = "../images/down.png";
+        if (arrow.getAttribute("src") === "images/up.png") {
+            arrow.src = "images/down.png";
         }
         else {
-            arrow.src = "../images/up.png";
+            arrow.src = "images/up.png";
         }
 
         // Change whiteboard
